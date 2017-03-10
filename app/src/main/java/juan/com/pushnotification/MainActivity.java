275116cc521974d3485c4b055ac4b6cedcc646b6
Juan <cucsijuan@gmail.com>
2017-03-10 20:35:56 -0300

    primer commit

---
+++ app/src/main/java/juan/com/pushnotification/MainActivity.java
@@ -0,0 +1,32 @@
+package juan.com.pushnotification;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.notificar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(MainActivity.this);
+                nBuilder.setContentTitle("Kernel panic!");
+                nBuilder.setContentText("Printer on fire!!");
+                nBuilder.setSmallIcon(android.R.drawable.ic_dialog_alert);
+                NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+                Notification notif = nBuilder.build();
+                notificationManager.notify(2233,notif);
+            }
+        });
+    }
+}
